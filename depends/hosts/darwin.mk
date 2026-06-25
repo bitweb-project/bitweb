314da8e3576108f3a5a29135173d7177af54625d
mraksoll4 <32709596+mraksoll4@users.noreply.github.com>
2026-06-25 10:27:48 +0300

    Fix ad-hoc codesign
    Tree-SHA512: 2e40622247b128c18b992c15e75435ae891357cee7695a628d3f360c7d00d46b9243e6f16b1b9f5491ca5691a11fa339502d9dc3fd75e56fe3280750ef126011

--- depends/hosts/darwin.mk
+++ depends/hosts/darwin.mk
@@ -72,7 +72,7 @@ darwin_LDFLAGS=-Wl,-platform_version,macos,$(OSX_MIN_VERSION),$(OSX_SDK_VERSION)
 ifneq ($(build_os),darwin)
 darwin_CFLAGS += -mlinker-version=$(LLD_VERSION)
 darwin_CXXFLAGS += -mlinker-version=$(LLD_VERSION)
-darwin_LDFLAGS += -Wl,-no_adhoc_codesign -fuse-ld=lld
+darwin_LDFLAGS += -fuse-ld=lld
 endif
 
 darwin_release_CFLAGS=-O2
